61b62b06378e3991af27f7b6b865d54bf9b54a99
maizse <72534845+maizse@users.noreply.github.com>
2021-02-08 14:47:01 +0800

    Update and rename number_occurences.py to number_occurrences.py

---
+++ number_occurrences.py
@@ -0,0 +1,12 @@
+
+array_numbers = [1,2,3,4,3,2,3,3,2]
+print("Original array: " +str(array_numbers))
+print("Get the number of occurrences of number: ")
+x = int(input())
+print("Number of occurrences of " + str(x) + ": " + str(array_numbers.count(x)))
+
+array_strings = ["car", "bike", "toy", "car", "car"]
+print("Array of strings: " +str(array_strings))
+print("Get the number of occurrences of : ")
+y = input()
+print("Number of occurrences of " + y + ": " + str(array_strings.count(y)))
